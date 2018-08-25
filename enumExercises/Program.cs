5fd79c172a23fcf819d5ab93611fef0d6345c114
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-25 14:04:10 -0700

    enumExercises
    draft with errors. not sure how to write catch statements to catch exceptions for when user enters either a number or some string not equal to any of the days of the week

--- enumExercises/Program.cs
+++ enumExercises/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,45 @@ namespace enumExercises
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Please type in the current day of the week.");
+                string userInput = Console.ReadLine();
+                foreach (string day in Enum.GetNames(typeof(daysOfTheWeek)))
+                {
+                    if (userInput == day)
+                    {
+                        Console.WriteLine("It is {0}.", day);
+                        
+                    }
+                   
+                }
+            }
+            catch (FormatException) //?? not sure which prewritten exception would handle for non-string input or string input not equal to  day in enum daysOfTheWeek
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+
+            }
+            Console.ReadLine();
+
         }
+        public enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+
+
+        }
+
+        
     }
 }
