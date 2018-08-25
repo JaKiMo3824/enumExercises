0807d4520ceea11985297548bfa3bd62868753ab
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-25 12:22:28 -0700

    Add project files.

---
+++ enumExercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
